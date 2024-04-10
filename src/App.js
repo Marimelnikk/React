fb6e72fd9efc37b801c250466e6c53fcdf69b0d4
Marina Melnikova <mari.melnikova010@gmail.com>
2024-04-10 20:46:19 +0300

    weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
+function App() {
+  const [weather, setWeather] = useState(null);
 
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
+  useEffect(() => {
+    const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
+    const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'ac07a20f5emsh701def20c4e8556p101077jsnee557b484313',
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    }
   };
 
-  render() {
-    return (
-      <div>
-        <span>{this.state.count}</span>
-        <button onClick={this.increment}>Click</button>
-      </div>
-    );
-  }
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => setWeather(data))
+  }, []);
+
+  return (
+    <div>{JSON.stringify(weather)}</div>
+  );
 }
 
 export default App;
