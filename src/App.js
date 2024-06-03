00c1ec647b73378ee7fedcf78842823ed964e29e
Marina Melnikova <mari.melnikova010@gmail.com>
2024-06-03 22:27:08 +0300

    catFacts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
-function App() {
-  const [weather, setWeather] = useState(null);
+const App = () => {
+  const [catFacts, setCatFacts] = useState(null);
 
-  useEffect(() => {
-    const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
-    const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'ac07a20f5emsh701def20c4e8556p101077jsnee557b484313',
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    }
+  const fetchCatFacts = () => {
+    fetch('https://catfact.ninja/fact')
+      .then(response => response.json())
+      .then(data => setCatFacts(data.fact))
   };
 
-    fetch(url, options)
-      .then(response => response.json())
-      .then(data => setWeather(data))
+  useEffect(() => {
+    fetchCatFacts();
   }, []);
 
+  const handleOnClick = () => {
+    fetchCatFacts();
+  };
+  
   return (
-    <div>{JSON.stringify(weather)}</div>
+    <>
+    <p>{catFacts}</p>
+    <button onClick={handleOnClick}>More</button>
+    </>
   );
 }
 
